6f4734afbd6ffb1cf8dace8e0e79aaecc8de63cb
icydwei <robpaik@gmail.com>
2019-10-30 10:22:23 -0400

    Done.

--- lib/multiples.rb
+++ lib/multiples.rb
@@ -1,1 +1,29 @@
 # Enter your procedural solution here!
+
+
+#up to 999
+#I could use a for loop that does <1000 and goes through each
+#number if modulo is 0 then shovel that into an array
+#then sum up all values in that array
+
+
+
+def collect_multiples(limit)
+  multiples = []
+
+  for i in 1...limit do
+    if (i % 5 == 0 || i % 3 == 0)
+      multiples << i
+    end
+  end
+
+  return multiples
+
+end
+
+def sum_multiples(limit)
+  sum = 0
+  multiples = collect_multiples(limit)
+  multiples.each{|multiple| sum += multiple}
+  return sum
+end
--- lib/oo_multiples.rb
+++ lib/oo_multiples.rb
@@ -1,1 +1,29 @@
 # Enter your object-oriented solution here!
+class Multiples
+
+  attr_accessor :limit
+
+  def initialize(limit)
+    @limit = limit
+  end
+
+  def collect_multiples
+    multiples = []
+    for i in 1...(self.limit) do
+      if (i % 5 == 0 || i % 3 == 0)
+        multiples << i
+      end
+    end
+
+    return multiples
+  end
+
+  def sum_multiples
+    sum = 0
+    multiples = self.collect_multiples
+    multiples.each{|multiple| sum += multiple}
+    return sum
+  end
+
+
+end
