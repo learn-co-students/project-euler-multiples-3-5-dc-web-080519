efb0801f1a3518ca9f34f64717b7975695ab08cc
Marisa Canan-Zucker <mcananzucker@gmail.com>
2019-12-08 14:31:52 -0500

    object-oriented solution

--- lib/oo_multiples.rb
+++ lib/oo_multiples.rb
@@ -1,1 +1,29 @@
 # Enter your object-oriented solution here!
+class Multiples
+
+  attr_reader :limit
+
+  def initialize(limit)
+    @limit = limit
+  end
+
+  def collect_multiples
+    count = 3
+    multiples = []
+
+    while count < self.limit do
+      if count % 3 == 0 || count % 5 == 0
+        multiples << count
+      end
+      count += 1
+    end
+
+    return multiples
+  end
+
+  def sum_multiples
+    multiples = self.collect_multiples
+    multiples.reduce(:+)
+  end
+
+end
